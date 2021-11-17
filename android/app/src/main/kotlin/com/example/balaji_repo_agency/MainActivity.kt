9d830798958b6cf880be4e2a5dc9da78d22ebcdb
VINEET KUMAR SHARMA <vineetkumarsharma17@gmail.com>
2021-11-17 22:01:50 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/balaji_repo_agency/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.balaji_repo_agency
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ,
+  ));
+}
+class SpalshPage extends StatelessWidget {
+  const SpalshPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Splash page"),
+      ),
+    );
+  }
+}
+
