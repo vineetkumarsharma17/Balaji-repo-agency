bcf27597b8bf6f5d85e752697303482a845dd51e
VINEET KUMAR SHARMA <vineetkumarsharma17@gmail.com>
2021-11-17 22:59:32 +0530

    mobile login page added commit

--- lib/screens/mobile_login.dart
+++ lib/screens/mobile_login.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 class LoginMobile extends StatefulWidget {
   const LoginMobile({Key? key}) : super(key: key);
@@ -20,81 +21,85 @@ class _LoginMobileState extends State<LoginMobile> {
                 image: AssetImage("assets/images/bg_blur.jpg")
             )
         ),
-        child: Column(
-          children: [
-            Expanded(child: Container(
-              alignment: Alignment.centerLeft,
-              width: MediaQuery.of(context).size.width,
-              // color: Colors.red,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 68.0),
-                child: Text("Welcome!!",style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold
+        child: Container(
+          margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*.4,),
+       //  height: MediaQuery.of(context).size.height*.7,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(40),
+                  topRight:  Radius.circular(40))
+          ),
+          child: Column(
+            children: [
+              Center(
+                heightFactor: 3,
+                child: Text("Sign In",style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: Colors.deepPurple
                 ),),
               ),
-            )),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(40),
-                      topRight:  Radius.circular(40))
-              ),
-              child: Column(
-                children: [
-                  Center(
-                    heightFactor: 2,
-                    child: Text("Sign In",style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
-                        color: Colors.deepPurple
-                    ),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*.1),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.teal,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: TextFormField(
-                        style: TextStyle(color: Colors.white),
-                        controller: mobile,
-                        decoration: InputDecoration(
-                            icon: Icon(Icons.email_outlined,color: Colors.white,),
-                            hintText: "Mobile",
-                            hintStyle: TextStyle(
-                                color: Colors.white70
-                            ),
-                            border: InputBorder.none
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*.1),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.teal,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: TextFormField(
+                    style: TextStyle(color: Colors.white),
+                    controller: mobile,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.email_outlined,color: Colors.white,),
+                        hintText: "Mobile",
+                        hintStyle: TextStyle(
+                            color: Colors.white70
                         ),
-                      ),
+                        border: InputBorder.none
                     ),
                   ),
-                  ElevatedButton(onPressed: (){
-                  //  Navigator.push(context, MaterialPageRoute(builder: (context)=>()));
-
-                  }, child: Text("Sign In",style: TextStyle(
-                      fontSize: 17
-                  ),),
-                    style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(Size(120, 40)),
-                        backgroundColor: MaterialStateProperty.all(Colors.teal),
-                        shape:MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius:BorderRadius.circular(20),
-                            side: BorderSide.none
-                        ))
-                    ),),
-                ],
+                ),
               ),
+              SizedBox(height: 10,),
+              ElevatedButton(onPressed: (){
+              //  Navigator.push(context, MaterialPageRoute(builder: (context)=>()));
+                if(mobile.text.isNotEmpty){
+                    sendOTP();
+                  }
+                }, child: Text("Send OTP",style: TextStyle(
+                  fontSize: 17
+              ),),
+                style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(Size(120, 40)),
+                    backgroundColor: MaterialStateProperty.all(Colors.teal),
+                    shape:MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius:BorderRadius.circular(20),
+                        side: BorderSide.none
+                    ))
+                ),),
+            ],
+          ),
 
-            ),
-          ],
         ),
       ),
+    );
+  }
+  void sendOTP()async{
+      await FirebaseAuth.instance.verifyPhoneNumber(
+      phoneNumber: '+91'+mobile.text,
+      verificationCompleted: (PhoneAuthCredential credential) {},
+      verificationFailed: (FirebaseAuthException e) {
+        print(e);
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        print("code sent to "+mobile.text);
+        print(verificationId);
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {
+        print("Timeout");
+      },
     );
   }
 }
